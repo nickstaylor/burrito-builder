1353607bf6805ed2e53322c6e6856619625ce136
Nick Taylor <nickstaylor@gmail.com>
2020-09-22 13:09:08 -0600

    Add POST method for new burrito

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,18 @@ export const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders')
       .then(response => response.json())
 }
+
+export const takeOrder = (order) => {
+  return fetch("http://localhost:3001/api/v1/orders", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      id: order.id,
+      name: order.name,
+      ingredients: order.ingredients
+    })
+  })
+    .then((response => response.json()));
+}
