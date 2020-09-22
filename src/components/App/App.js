897ab17ec67795ea4c219ed5b6b26b4e79d6832b
Nick Taylor <nickstaylor@gmail.com>
2020-09-22 13:10:42 -0600

    Add API get/post methods for retrieving orders and placing orders

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
-import {getOrders} from '../../apiCalls';
+import {getOrders, takeOrder, deleteOrder} from '../../apiCalls';
 import Orders from '../../components/Orders/Orders';
 import OrderForm from '../../components/OrderForm/OrderForm';
 
 class App extends Component {
   constructor(props) {
     super();
+    this.state = {
+      orders: []
+    }
   }
 
   componentDidMount() {
     getOrders()
+      .then(data => {
+        this.setState({
+          orders: data.orders
+        })
+      })
       .catch(err => console.error('Error fetching:', err));
   }
+
+  takeAnOrder = (order) => {
+    takeOrder(order)
+    .then(data => this.setState({orders: [...this.state.orders, data]}))
+  }
+
+
 
   render() {
     return (
       <main className="App">
         <header>
           <h1>Burrito Builder</h1>
-          <OrderForm />
+          <OrderForm takeAnOrder={this.takeAnOrder}/>
         </header>
 
         <Orders orders={this.state.orders}/>
