3d32201130c5baca1e2bc2891ad4397919b4a105
Nick Taylor <nickstaylor@gmail.com>
2020-09-24 17:23:07 -0600

    Test written for delete order on App.test file.  Mocked out fetch return and test is passing

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import App from './App'
 import { fireEvent, render, waitFor } from "@testing-library/react"
 import "@testing-library/jest-dom"
-import { takeOrder, getOrders } from '../../apiCalls'
+import { takeOrder, getOrders, deleteOrder } from '../../apiCalls'
 jest.mock("../../apiCalls");
 
 describe("App", () => {
@@ -54,7 +54,7 @@ describe("App", () => {
     it('should be able to add a burrito order and display the new order', async () => {
         await getOrders.mockResolvedValueOnce(sampleOrders)
         await takeOrder.mockResolvedValueOnce({
-                        id: 2,
+                        id: 3,
                         name: "Ted",
                         ingredients: ["beans", "steak"]
    
@@ -83,6 +83,38 @@ describe("App", () => {
       });
       expect(errorMessage).toBeInTheDocument();
     });
+
+
+
+    it("should be able to delete and order", async () => {
+        await getOrders.mockResolvedValueOnce(sampleOrders);
+        await takeOrder.mockResolvedValueOnce({
+            id: 374,
+            name: "Ted",
+            ingredients: ["beans", "steak"],
+          });
+        await deleteOrder.mockResolvedValueOnce({ ok: true })
+        
+        const { getByText, getByRole, getByPlaceholderText, debug } = render(<App />);
+
+        fireEvent.change(getByPlaceholderText("Name"), {target: { value: "Ted" },});
+        const beans = getByRole("button", { name: "beans" });
+        fireEvent.click(beans);
+        const steak = getByRole("button", { name: "steak" });
+        fireEvent.click(steak);
+        fireEvent.click(getByText("Submit Order"));
+        const tedName = await waitFor(() => getByText("Ted"));
+        expect(tedName).toBeInTheDocument();
+        // debug()
+        const button = await waitFor(()=> getByRole("374"));
+        fireEvent.click(button);
+    
+        expect(deleteOrder).toHaveBeenCalled();
+        expect(deleteOrder).toHaveBeenCalledWith(374);
+        await waitFor(()=> expect(tedName).not.toBeInTheDocument())
+        debug()
+    
+    })
 
 
 
